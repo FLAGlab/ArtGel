d9f416b2ab1e0ccf3ac73eb942f13b7ba13dd6e2
ha-ruiz75 <34463016+ha-ruiz75@users.noreply.github.com>
2019-03-07 18:03:59 -0500

    Update DistributionState.java

--- src/Geles/DistributionState.java
+++ src/Geles/DistributionState.java
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * ArtGel - Artificial Intelligence Gel Analysis Tool
+ * Copyright 2019 Hector A. Ruiz-Moreno, Cindy P. Ulloa-Guerrero, Jorge Duitama
+ *
+ * This file is part of ArtGel.
+ *
+ *     ArtGel is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     NGSEP is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with NGSEP.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package Geles;
 
 import ngsep.hmm.HMMState;
